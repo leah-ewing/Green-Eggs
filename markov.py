e48f7489a927f246620997b113b0be3fb02ea25c
Leah Ewing <leahewing@live.com>
2021-05-04 21:44:14 +0000

    Committing for the day - see comment for picking back up tomorrow

--- markov.py
+++ markov.py
@@ -19,6 +19,33 @@ working_file = open_and_read_file('green-eggs.txt')
 # print(working_file)
 
 
+example_dict = {('a', 'fox?'): ['Would'],
+ ('Sam', 'I'): ['am?'],
+ ('could', 'you'): ['in', 'with', 'in', 'with'],
+ ('you', 'with'): ['a', 'a'],
+ ('box?', 'Would'): ['you'],
+ ('ham?', 'Would'): ['you'],
+ ('you', 'in'): ['a', 'a'],
+ ('a', 'house?'): ['Would'],
+ ('like', 'green'): ['eggs'],
+ ('like', 'them,'): ['Sam'],
+ ('and', 'ham?'): ['Would'],
+ ('Would', 'you'): ['could', 'could', 'could', 'could', 'like', 'like'],
+ ('you', 'could'): ['you', 'you', 'you', 'you'],
+ ('a', 'mouse?'): ['Would'],
+ ('them,', 'Sam'): ['I'],
+ ('in', 'a'): ['house?', 'box?'],
+ ('with', 'a'): ['mouse?', 'fox?'],
+ ('house?', 'Would'): ['you'],
+ ('a', 'box?'): ['Would'],
+ ('green', 'eggs'): ['and'],
+ ('you', 'like'): ['green', 'them,'],
+ ('mouse?', 'Would'): ['you'],
+ ('fox?', 'Would'): ['you'],
+ ('eggs', 'and'): ['ham?']
+}
+
+
 def make_chains(text_string):
     """Take input text as string; return dictionary of Markov chains.
 
@@ -47,13 +74,28 @@ def make_chains(text_string):
     chains = {}
 
     word = text_string.split()
-
+    
     for i in range(len(word) - 2):
+        #test_chains = []
         test_list = []
-        # test_list.append(word[i + 2])
+        key = (word[i], word[i + 1])
+        value = [] #appending new words value list (see below)
+        #test_list.append(word[i + 2])
         chains[ word[i], word[i + 1] ] = test_list
         test_list.append(word[i + 2])
-    
+        if chains.get(key, False):
+            
+        # each value in dict, "chains" should be a list
+        # check if key in dict, if it is, append new word to value (that is a list) for key 
+
+        # d[key] = d.get(key, 0) + 1
+        #test_chains.append(chains[ word[i], word[i + 1] ])
+        #print(test_list)
+        # if chains[ word[i], word[i + 1] ] in test_list:
+        #     print('it executes!')
+            #test_list.append(word[i + 2])
+        #     chains[]
+    return(chains)
         
 
     # for j in range(len(word) -2, -1):
@@ -61,7 +103,7 @@ def make_chains(text_string):
     #     #test_list = []
     #     #test_list.append(word[i + 2])
     #     chains[ word[j], word[j + 1] ] = (word[0]) # value should be first word in list
-    return(chains)
+    
         
 
 # create empty dictionary
@@ -75,13 +117,15 @@ def make_chains(text_string):
 
     # print(word)
 
-        # print(chains)
+        #print(chains)
 
     # print('*'* 10)
-    print(chains)
+    #print(chains.keys())
 
 test_function = make_chains(working_file)
 print(test_function)
+
+#print(test_function is example_dict)
 
 def make_text(chains):
     """Return text from chains."""
